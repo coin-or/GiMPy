1bcf1dc7cc1c517c2f4555104fe006a1809f533d
Aykut Bulut <aykut@lehigh.edu>
2013-01-01 05:06:10 +0000

    simplex test scripts added.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@706 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

---
+++ examples/simplex_test.py
@@ -0,0 +1,256 @@
+'''
+tests network simplex method and cycle canceling method of GIMPy.
+
+'''
+
+from gimpy import Graph
+from random import seed, randint, random
+from pulp import LpProblem, LpVariable, LpMinimize, lpSum, value, GLPK
+import time
+
+# global variables, experimental parameters
+capacity_range = (10,20)
+cost_range = (30,50)
+demand_range = (10,15)
+demand_numnodes = 3
+supply_numnodes = 2
+numnodes = 6
+density = 0.4
+
+#testing
+# nodes       10, 15, 20, 25, 30
+# arcs sparse 20, 30, 45, 65, 90
+# arcs dense  35, 80, 150, 200, 300
+
+def get_solution(g):
+    '''
+    returns optimal solution (optimal flows) of min cost flow problem for a
+    given graph instance g. return type is dictionary, keys are edge keys values
+    are int.
+    '''
+    sol = {}
+    for e in g.get_edge_list():
+        sol[e] = g.get_edge_attr(e[0],e[1],'flow')
+    return sol
+
+def generate_graph(seed_i):
+    g = mGraph(graph_type='digraph', splines='true', layout = 'dot')
+    #g = mGraph(graph_type='digraph')
+    seed(seed_i)
+    for i in range(numnodes):
+        for j in range(numnodes):
+            if i==j:
+                continue
+            if (str(i), str(j)) in g.get_edge_list():
+                continue
+            if (str(j), str(i)) in g.get_edge_list():
+                continue
+            if random() < density:
+                cap = randint(capacity_range[0], capacity_range[1])
+                cos = randint(cost_range[0], cost_range[1])
+                g.add_edge(i, j, cost=cos, capacity=cap)
+    # set supply/demand
+    # select random demand_numnodes many nodes
+    demand_node = {}
+    while len(demand_node) < demand_numnodes:
+        n = str(randint(0,numnodes-1))
+        if n in demand_node:
+            continue
+        demand_node[n] = 0
+    # select random supply_numnodes many nodes (different than above)
+    supply_node = {}
+    while len(supply_node) < supply_numnodes:
+        n = str(randint(0,numnodes-1))
+        if n in demand_node or n in supply_node:
+            continue
+        supply_node[n] = 0
+    # set demand amounts
+    total_demand = 0
+    for n in demand_node:
+        demand_node[n] = randint(demand_range[0], demand_range[1])
+        total_demand += demand_node[n]
+    # set supply amounts
+    total_supply = 0
+    for n in supply_node:
+        supply_node[n] = randint(demand_range[0], demand_range[1])
+        total_supply += supply_node[n]
+    if total_demand > total_supply:
+        # this line may create random behavior, because of dictionary query
+        for n in supply_node:
+            excess = total_demand-total_supply
+            supply_node[n] += excess
+            break
+    elif total_demand < total_supply:
+        for n in demand_node:
+            excess = total_supply-total_demand
+            demand_node[n] += excess
+            break
+    # set demand attributes
+    for n in g.get_node_list():
+        if n in demand_node:
+            g.set_node_attr(n, 'demand', -1*demand_node[n])
+        elif n in supply_node:
+            g.set_node_attr(n, 'demand', supply_node[n])
+        else:
+            g.set_node_attr(n, 'demand', 0)
+    return g, demand_node, supply_node
+
+def get_obj_value(g):
+    el = g.get_edge_list()
+    cost = 0
+    for e in el:
+        cost_e = g.get_edge_attr(e[0], e[1], 'cost')
+        flow_e = g.get_edge_attr(e[0], e[1], 'flow')
+        cost += cost_e*flow_e
+    return cost
+
+def solve(g):
+    el = g.get_edge_list()
+    nl = g.get_node_list()
+    p = LpProblem('min_cost', LpMinimize)
+    capacity = {}
+    cost = {}
+    demand = {}
+    x = {}
+    for e in el:
+        capacity[e] = g.get_edge_attr(e[0], e[1], 'capacity')
+        cost[e] = g.get_edge_attr(e[0], e[1], 'cost')
+    for i in nl:
+        demand[i] = g.get_node_attr(i, 'demand')
+    for e in el:
+        x[e] = LpVariable("x"+str(e), 0, capacity[e])
+    # add obj
+    objective = lpSum (cost[e]*x[e] for e in el)
+    p += objective
+    # add constraints
+    for i in nl:
+        out_neig = g.get_out_neighbors(i)
+        in_neig = g.get_in_neighbors(i)
+        p += lpSum(x[(i,j)] for j in out_neig) -\
+             lpSum(x[(j,i)] for j in in_neig)==demand[i]
+    p.solve(GLPK(msg = 0))
+    return x, value(objective)
+
+class mGraph(Graph):
+
+    def __init__(self, display = 'off', dot_data = None, 
+                 dot_file = None, **attrs):
+        Graph.__init__(self, display, dot_data, dot_file, **attrs)
+
+    def network_simplex(self, display, pivot):
+        '''
+        API:
+            network_simplex(self, display, pivot)
+        Description:
+            Solves minimum cost feasible flow problem using network simplex
+            algorithm. It is recommended to use min_cost_flow(algo='simplex')
+            instead of using network_simplex() directly. Returns True when an
+            optimal solution is found, returns False otherwise. 'flow' attribute
+            values of arcs should be considered as junk when returned False.
+        Pre:
+            (1) check Pre section of min_cost_flow()
+        Inputs:
+            pivot: specifies pivot rule. Check min_cost_flow()
+            display: 'off' for no display, 'pygame' for live update of
+            spanning tree.
+        Post:
+            (1) Changes 'flow' attribute of edges.
+        Return:
+            Returns True when an optimal solution is found, returns
+            False otherwise.
+        '''
+        # ==== determine an initial tree structure (T,L,U)
+        # find a feasible flow
+        iter = 0
+        if not self.find_feasible_flow():
+            return (False, iter)
+        t = self.simplex_find_tree()
+        # mark spanning tree arcs
+        self.simplex_mark_st_arcs(t)
+        self.set_display_mode(display)
+        # display initial spanning tree
+        self.display()
+        self.set_display_mode('off')
+        # set predecessor, depth and thread indexes
+        t.simplex_search('1', 1)
+        # compute potentials
+        self.simplex_compute_potentials(t)
+        # while some nontree arc violates optimality conditions
+        while not self.simplex_optimal(t):
+            self.set_display_mode(display)
+            self.display()
+            self.set_display_mode('off')
+            # select an entering arc (k,l)
+            (k,l) = self.simplex_select_entering_arc(t, pivot)
+            self.simplex_mark_entering_arc(k, l)
+            # determine leaving arc
+            ((p,q), capacity, cycle)=self.simplex_determine_leaving_arc(t,k,l)
+            # mark leaving arc
+            self.simplex_mark_leaving_arc(p, q)
+            self.set_display_mode(display)
+            # display after marking leaving arc
+            self.display()
+            self.simplex_mark_st_arcs(t)
+            self.display()
+            self.set_display_mode('off')
+            # remove arc
+            self.simplex_remove_arc(t, p, q, capacity, cycle)
+            # set predecessor, depth and thread indexes
+            t.simplex_search('1', 1)
+            # compute potentials
+            self.simplex_compute_potentials(t)
+        return (True, iter)
+
+if __name__=='__main__':
+    print
+    dantzig_file = open('dantzig_results.txt', 'w')
+    eligible_file = open('eligible_results.txt', 'w')
+    dantzig_file.write('# seed, simplex obj value, pulp obj value,'+\
+                           ' time, iteration\n')
+    eligible_file.write('# seed, simplex obj value, pulp obj value,'+\
+                            ' time, iteration\n')
+    for seed_i in range(0,100):
+        g, d, s = generate_graph(seed_i)
+        #==========solve using simplex, first eligible
+        start = time.clock()
+        tup = g.network_simplex('pygame', 'first_eligible')
+        elapsed_time = time.clock() - start
+        if tup[0]:
+            sol = get_solution(g)
+            eligible_obj_value = get_obj_value(g)
+        else:
+            # skip infeasible ones
+            continue
+            # set obj value to 0 if the problem is infeasible.
+            eligible_obj_value = 0.0
+        #==========solve using pulp
+        x, pulp_obj_value = solve(g)
+        pulp_obj_value = int(pulp_obj_value)
+        # check success of simplex with first eligible
+        flag = True
+        if eligible_obj_value != pulp_obj_value:
+            flag = False
+        e_line = str(seed_i).ljust(5)+str(eligible_obj_value).ljust(6)+\
+            str(pulp_obj_value).ljust(6)+str(elapsed_time).ljust(7)+str(tup[1])+'\n'
+        eligible_file.write(e_line)
+        #==========solve using simplex, dantzig
+        start = time.clock()
+        tup = g.network_simplex('pygame', 'dantzig')
+        elapsed_time = time.clock() - start
+        if tup[0]:
+            sol = get_solution(g)
+            dantzig_obj_value = get_obj_value(g)
+        else:
+            # set obj value to 0 if the problem is infeasible.
+            simplex_obj_value = 0.0
+        # write to file
+        d_line = str(seed_i).ljust(5)+str(dantzig_obj_value).ljust(6)+\
+            str(pulp_obj_value).ljust(6)+str(elapsed_time).ljust(7)+str(tup[1])+'\n'
+        dantzig_file.write(d_line)
+        flag = True
+        if dantzig_obj_value != pulp_obj_value:
+            flag = False
+    dantzig_file.close()
+    eligible_file.close()
+    if flag:
+        print 'All problems solved accuretaly.'
---
+++ examples/simplex_visualization_test.py
@@ -0,0 +1,67 @@
+'''
+tests network simplex visualization
+'''
+
+from gimpy import Graph
+from random import seed, randint, random
+
+def generate_graph():
+    g = Graph(graph_type='digraph')
+    # supply nodes; 1,2
+    g.add_node(1, demand=20, pos='"0,3!"')
+    g.add_node(2, demand=10, pos='"0,1!"')
+    # transhipment nodes; 3,4
+    g.add_node(3, demand=0, pos='"3,4!"')
+    g.add_node(4, demand=0, pos='"3,1!"')
+    # demand nodes; 5,6,7
+    g.add_node(5, demand=-5, pos='"6,3!"')
+    g.add_node(6, demand=-10, pos='"6,2!"')
+    g.add_node(7, demand=-15, pos='"6,0!"')
+    # add edges
+    g.add_edge(1,2,cost=5,capacity=10)
+    g.add_edge(1,3,cost=6,capacity=15)
+    g.add_edge(1,4,cost=10,capacity=15)
+    g.add_edge(2,3,cost=7,capacity=10)
+    g.add_edge(3,4,cost=4,capacity=15)
+    g.add_edge(3,5,cost=5,capacity=10)
+    g.add_edge(3,6,cost=7,capacity=10)
+    g.add_edge(4,5,cost=6,capacity=8)
+    g.add_edge(4,6,cost=4,capacity=10)
+    g.add_edge(4,7,cost=8,capacity=20)
+    g.add_edge(5,6,cost=2,capacity=10)
+    g.add_edge(6,7,cost=2,capacity=20)
+    return g
+
+def generate_graph2():
+    '''
+    this example is form lecture notes of Prof. James Orlin in Optimization
+    Methods in Management Science class. Link to lecture notes is as follows.
+    http://ocw.mit.edu/courses/sloan-school-of-management/
+    15-053-optimization-methods-in-management-science-spring-2007/
+    lecture-notes/lec15.pdf
+    '''
+    g = Graph(graph_type='digraph')
+    # supply nodes; 2,3,6
+    g.add_node(2, demand=6, pos='"1,2!"')
+    g.add_node(3, demand=6, pos='"0,0!"')
+    g.add_node(6, demand=12, pos='"5,2!"')
+    # demand nodes; 1,4,5,7
+    g.add_node(1, demand=-3, pos='"3,4!"')
+    g.add_node(4, demand=-11, pos='"2,0!"')
+    g.add_node(5, demand=-3, pos='"3,2!"')
+    g.add_node(7, demand=-7, pos='"4,0!"')
+    # add edges
+    g.add_edge(1,2,cost=1,capacity=10)
+    g.add_edge(2,4,cost=2,capacity=15)
+    g.add_edge(2,5,cost=2,capacity=15)
+    g.add_edge(3,2,cost=2,capacity=10)
+    g.add_edge(3,4,cost=1,capacity=4)
+    g.add_edge(4,7,cost=1,capacity=10)
+    g.add_edge(6,1,cost=3,capacity=10)
+    g.add_edge(6,5,cost=2,capacity=2)
+    g.add_edge(6,7,cost=10,capacity=5)
+    return g
+
+if __name__=='__main__':
+    g = generate_graph()
+    g.network_simplex('pygame', 'first_eligible')
