5533f8d33844232c772ab4c35fb0ac9c22b952bb
Ted Ralphs <ted@lehigh.edu>
2013-02-14 16:29:53 +0000

    Updating module constructor
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@744 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 import gimpy, Stack, LinkedList, Queues
-from gimpy import Graph, Subgraph, Cluster, Tree, BinaryTree, DisjointSet, BBTree
+from gimpy import Graph, Subgraph, Cluster, Tree, BinaryTree, DisjointSet, BBTree, etree_installed, pygame_installed, gexf_installed, PIL_installed, xdot_installed 
