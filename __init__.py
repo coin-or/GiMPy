e2e4119e3cadd8c64ec8154bbf5425ab8004d189
tkr <tkr@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-03-22 06:06:11 +0000

    Formatting
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@825 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 import gimpy, Stack, LinkedList, Queues
-from gimpy import Graph, Subgraph, Cluster, Tree, BinaryTree, DisjointSet, BBTree, etree_installed, pygame_installed, gexf_installed, PIL_installed, xdot_installed 
+from gimpy import Graph, Subgraph, Cluster, Tree, BinaryTree, DisjointSet, etree_installed, pygame_installed, PIL_installed, xdot_installed, quote_if_necessary 
+
