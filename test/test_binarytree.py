5084ab43ae41addd180fc72d6f0c83eb9a614ed9
aykut <aykut@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-04-17 08:02:03 +0000

    test directory created. All scripts related to testing moved to test.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/branches/reimplementation@870 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

---
+++ test/test_binarytree.py
@@ -0,0 +1,11 @@
+from gimpy import BinaryTree
+import random
+
+if __name__=='__main__':
+    t = BinaryTree(display='pygame')
+    t.add_root(0)
+    t.add_left_child(1, 0, label='lc', color='red')
+    t.add_right_child(2, 0, label='rc', color='red')
+    t.add_right_child(3, 2, label='rrc', color='green')
+    t.dfs()
+
