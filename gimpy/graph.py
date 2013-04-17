816d3d9b4ed8436884d528cd1b465297d2175dbd
Aykut Bulut <aykut@lehigh.edu>
2013-04-17 07:53:04 +0000

    moved DisjointSet class to graph.py and removed disjoint_set.py. Both classes depend on each other. Graph uses DisjointSet for Kruskal and DisjointSet inherits Graph, it is reasonable for both of them share the same module.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/branches/reimplementation@868 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- gimpy/graph.py
+++ gimpy/graph.py
@@ -72,7 +72,6 @@ get_attr() method. Should we change this?
 from global_constants import *
 from basic_structures import Stack
 from basic_structures import Queue, PriorityQueue
-from disjoint_set import DisjointSet
 import subprocess # for call()
 import StringIO   # for StringIO()
 import copy       # for deepcopy()
@@ -2409,6 +2408,47 @@ class Graph(object):
             if diff < min_delta:
                 break
         return pagerank
+
+
+class DisjointSet(Graph):
+    def __init__(self, optimize = True, **attrs):
+        attrs['type'] = DIRECTED_GRAPH
+        Graph.__init__(self, **attrs)
+        self.sizes = {}
+        self.optimize = optimize
+
+    def add(self, aList):
+        self.add_node(aList[0])
+        for i in range(1, len(aList)):
+            self.add_edge(aList[i], aList[0])
+        self.sizes[aList[0]] = len(aList)
+
+    def union(self, i, j):
+        roots = (self.find(i), self.find(j))
+        if roots[0] == roots[1]:
+            return False
+        if self.sizes[roots[0]] <= self.sizes[roots[1]] or not self.optimize:
+            self.add_edge(roots[0], roots[1])
+            self.sizes[roots[1]] += self.sizes[roots[0]]
+            return True
+        else:
+            self.add_edge(roots[1], roots[0])
+            self.sizes[roots[0]] += self.sizes[roots[1]]
+            return True
+
+    def find(self, i):
+        current = i
+        edge_list = []
+        while len(self.get_neighbors(current)) != 0:
+            successor = self.get_neighbors(current)[0]
+            edge_list.append((current, successor))
+            current = successor
+        if self.optimize:
+            for e in edge_list:
+                if e[1] != current:
+                    self.del_edge((e[0], e[1]))
+                    self.add_edge(e[0], current)
+        return current
 
 
 if __name__ == '__main__':
