096f7bf24362b5dd4e16c898179a323f45dbed03
Aykut Bulut <aykut@lehigh.edu>
2013-05-05 07:45:19 +0000

    __init__.py fixed. Now it sets importing related globals (PYGAME_INSTALLED, etc) correctly. xdot display mode fixed for gimpy.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/branches/reimplementation@916 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- test/test_display.py
+++ test/test_display.py
@@ -1,6 +1,22 @@
 from gimpy import Graph
 
 if __name__=="__main__":
-    g = Graph(display='PIL', layout='dot')
+    g = Graph(display='pygame', layout='dot')
     g.random(numnodes= 20, density =0.2)
-    g.display(basename='g', format='png')
+    # test pygame
+    g.display()
+    # test xdot
+    g.set_display_mode('xdot')
+    g.display()
+    # test file, layout dot
+    g.set_display_mode('file')
+    g.display(basename='test')
+    # test file, layout dot2tex
+    g.set_display_mode('file')
+    g.set_layout('dot2tex')
+    g.display()
+    # test PIL
+    g.set_layout('dot')
+    g.set_display_mode('PIL')
+    g.display()
+
