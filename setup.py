c3452ee0a8a5e26d014c6a7340b687bb7be08d82
tkr <tkr@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-05-26 19:39:39 +0000

    Adding standard files and setup.py script
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/branches/reimplementation@927 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Graph Methods in Python (GiMPy)',
+      author='Aykut Bulut, Ted Ralphs',
+      author_email='ayb',
+      url='http://projects.coin-or.org/CoinBazaar/wiki/Projects/GIMPy',
+      packages=['gimpy','blimpy'],
+      package_dir = {'': 'src'},
+     )
