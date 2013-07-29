6cb9c340d3cf17248d185339af93073ec64f57b2
aykut <aykut@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-07-29 06:33:40 +0000

    Final touches. It seems we will not provide test through setup.py. I could not find an up-to-date python documentation that explains how to acheive it. The ones I found were for python 2.4 or earlier and not supported anymore. We will provide test_*.py scripts in /test directory.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@959 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- test/test_display.py
+++ test/test_display.py
@@ -1,4 +1,31 @@
-from gimpy import Graph
+# import classes
+from gimpy import Graph, Tree, BinaryTree
+# import dependency related globals. All these dependencies are optional.
+# They all provide different display capabilities. If some of them are missing
+# it means you only miss the display capability provided by it.
+from gimpy import PYGAME_INSTALLED, DOT2TEX_INSTALLED, PIL_INSTALLED
+from gimpy import XDOT_INSTALLED, ETREE_INSTALLED
+
+if PYGAME_INSTALLED:
+    print 'Pygame is installed.'
+elif not PYGAME_INSTALLED:
+    print 'Warning: Pygame can not found.'
+if DOT2TEX_INSTALLED:
+    print 'Dot2tex is installed.'
+elif not DOT2TEX_INSTALLED:
+    print 'Warning: Dot2tex can not found.'
+if PIL_INSTALLED:
+    print 'PIL (Python Imaging Library) is installed.'
+elif not PIL_INSTALLED:
+    print 'Warning: PIL (Python Imaging Library) can not found.'
+if XDOT_INSTALLED:
+    print 'Xdot is installed.'
+elif not XDOT_INSTALLED:
+    print 'Warning: Xdot can not found.'
+if ETREE_INSTALLED:
+    print 'Etree is installed.'
+elif not ETREE_INSTALLED:
+    print 'Warning: Etree can not found.'
 
 if __name__=="__main__":
     g = Graph(display='off', layout='dot')
@@ -11,7 +38,7 @@ if __name__=="__main__":
     g.display()
     # test file, layout dot
     g.set_display_mode('file')
-    g.display(basename='test')
+    g.display(basename='test_graph')
     # test file, layout dot2tex
     g.set_display_mode('file')
     g.set_layout('dot2tex')
