7e8d8a0c7ac2e0fc8d14530f9dd10d67d5501096
Ted Ralphs <ted@lehigh.edu>
2013-08-04 19:48:51 +0000

    Adding some details to setup.py file
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@970 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='GiMPy',
-      version='1.0',
-      description='Graph Methods in Python (GiMPy)',
+      version='1.0.0',
+      description='Graph Methods in Python',
+      long_description='GiMPy is yet another graph class in Python. The class includes implementations of a range of standard graph algorithms. The goal of the implementations is clarity and the package includes visualizations for most of the methods. It is intended for educational/research purposes. The implementations are as efficient as possible, but where tradeoffs have to be made, clarity and ability to visualize are emphasized above efficiency. For this reason, the package is pure Python. Visualization is done using Graphviz, which should be installed in order to do any visualization. Graphs can be displayed using either pygame (live), PIL (statis), xdot (live), gexf (static display in browser), or dot2tex (set in LaTex).',
       author='Aykut Bulut, Ted Ralphs',
-      author_email='aykut@lehigh.edu',
+      author_email='{aykut,ted}@lehigh.edu',
+      license='Eclipse Public License',
       url='http://projects.coin-or.org/CoinBazaar/wiki/Projects/GIMPy',
       packages=['gimpy','blimpy'],
       package_dir = {'': 'src'},
