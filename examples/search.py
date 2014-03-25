5beb21d1e76a752967cd1f77db1d80ac6cffd3bd
tkr <tkr@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2014-03-25 13:50:27 +0000

    Adding search example
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/trunk@1132 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

---
+++ examples/search.py
@@ -0,0 +1,32 @@
+try:
+    from src.gimpy import Graph, UNDIRECTED_GRAPH
+except ImportError:
+    from coinor.gimpy import Graph, UNDIRECTED_GRAPH
+
+G = Graph(type = UNDIRECTED_GRAPH, splines = 'true', K = 1.5)
+G.random(numnodes = 7, density = 0.7, Euclidean = False, seedInput = 9)
+#G = Graph(type = DIRECTED_GRAPH, splines = 'true', K = 2)
+#    G.random(numnodes = 15, density = 0.4, degree_range=(1, 4), Euclidean = True, seedInput = 3)
+#    G.random(numnodes = 7, density = 0.7, length_range = (1, 10), seedInput = 5)
+#G.random(numnodes = 7, density = 0.7, Euclidean = True,
+#         seedInput = 9, add_labels = True)
+G.set_display_mode('pygame')
+G.display()
+#G.dfs(0)
+G.search(0, display = 'pygame', algo = 'BFS')
+        
+
+#    G.random(numnodes = 10, density = 0.5, seedInput = 5)
+
+#    G.set_display_mode('xdot')
+#    print G.to_string()
+#    G.display()
+#    G.display(basename='try.png', format='png')
+
+#    for i in G.nodes:
+#        G.nodes[i].set_attr('label', '-,-')
+#    G.dfs(0, display = 'pygame')
+#    G.search(0, display = 'pygame', algo = 'Dijkstra')
+#    G.minimum_spanning_tree_kruskal(display = 'pygame')
+#    G.search(0, display = 'pygame')
+#    G.cycle_canceling('pygame')
