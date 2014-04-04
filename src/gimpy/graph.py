f1e8af02ac54a54c9e9e5ca0201d8c66380ffbc3
Ted Ralphs <tkralphs@gmail.com>
2014-04-04 11:23:08 -0400

    Reverting a few changes from previous commit

--- src/gimpy/graph.py
+++ src/gimpy/graph.py
@@ -974,7 +974,10 @@ class Graph(object):
                 path.insert(0, pred[current])
                 current = pred[current]
             return path
-        return pred
+        if destination == None:
+            return pred
+        else:
+            return None
 
     def process_node_search(self, node, q, **kwargs):
         '''
@@ -3360,23 +3363,10 @@ class DisjointSet(Graph):
 
 
 if __name__ == '__main__':
-#    G = Graph(type = UNDIRECTED_GRAPH, splines = 'true', K = 1.5)
-#    G.random(numnodes = 7, density = 0.7, Euclidean = False, seedInput = 9)
     G = Graph(type = UNDIRECTED_GRAPH, splines = 'true', K = 1.5)
-#    G.random(numnodes = 15, density = 0.4, degree_range=(1, 4), Euclidean = True, seedInput = 3)
-#    G.random(numnodes = 7, density = 0.7, length_range = (1, 10), seedInput = 5)
     G.random(numnodes = 7, density = 0.7, Euclidean = True,
-             seedInput = 9, add_labels = True)
-#    G.random(numnodes = 10, density = 0.5, seedInput = 5)
+             seedInput = 9, add_labels = False)
+    G.set_display_mode('pygame')
+    G.display()
+    G.dfs(0)
 
-#    G.set_display_mode('xdot')
-#    print G.to_string()
-#    G.display()
-#    G.display(basename='try.png', format='png')
-
-#    for i in G.nodes:
-#        G.nodes[i].set_attr('label', '-,-')
-#    G.dfs(0, display = 'pygame')
-    G.search(0, display = 'pygame', algo = 'Dijkstra')
-#    G.minimum_spanning_tree_kruskal(display = 'pygame')
-#    G.search(0, display = 'pygame')
