31aca12ed5e6afeadd00e7ccc32befd91be18d7b
Ted Ralphs <ted@lehigh.edu>
2019-03-25 23:18:04 -0400

    Changing version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import setuptools
 
 setup(name='coinor.gimpy',
-      version='1.3.12',
+      version='2.0.0',
       description='Graph Methods in Python',
       long_description='''GiMPy is yet another graph class in Python. It is part of the COIN-OR software repository (www.coin-or.org). The class includes implementations of a range of standard graph algorithms. The goal of the implementations is clarity and the package includes visualizations for most of the methods. It is intended for educational/research purposes. The implementations are as efficient as possible, but where tradeoffs have to be made, clarity and ability to visualize are emphasized above efficiency. For this reason, the package is pure Python (and thus will not be as fast as other graph classes with C extensions). Visualization is done using Graphviz, which should be installed in order to do any visualization. Graphs can be displayed using either pygame (live), PIL (static), xdot (live), gexf (static display in browser), or dot2tex (set in LaTex).
 
@@ -13,11 +13,11 @@ Documentation for the API is here:
 https://coin-or.github.io/GiMPy
 ''',
       author='Aykut Bulut, Ted Ralphs',
-      author_email='{aykut,ted}@lehigh.edu',
+      author_email='ted@lehigh.edu',
       license='Eclipse Public License',
       url='https://github.com/coin-or/GiMPy/',
       namespace_packages=['coinor'],
       packages=['coinor.gimpy','coinor.gimpy.examples','coinor'],
       package_dir = {'coinor': 'src'},
-      install_requires=['coinor.blimpy>=1.2.0']
+      install_requires=['coinor.blimpy>=2.0.0']
      )
