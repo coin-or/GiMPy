42edb7e4db53892c0942fb6f99fd3edb67199ef4
Ted Ralphs <ted@lehigh.edu>
2020-01-15 20:52:42 -0500

    Increasing version number of release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import setuptools
 
 setup(name='coinor.gimpy',
-      version='2.0.1',
+      version='2.0.2',
       description='Graph Methods in Python',
       long_description='''GiMPy is yet another graph class in Python. It is part of the COIN-OR software repository (www.coin-or.org). The class includes implementations of a range of standard graph algorithms. The goal of the implementations is clarity and the package includes visualizations for most of the methods. It is intended for educational/research purposes. The implementations are as efficient as possible, but where tradeoffs have to be made, clarity and ability to visualize are emphasized above efficiency. For this reason, the package is pure Python (and thus will not be as fast as other graph classes with C extensions). Visualization is done using Graphviz, which should be installed in order to do any visualization. Graphs can be displayed using either pygame (live), PIL (static), xdot (live), gexf (static display in browser), or dot2tex (set in LaTex).
 
